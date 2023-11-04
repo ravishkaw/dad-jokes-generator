7c6627bf68ebae4cc2113227ee1417b430caf224
Ravishka Wijerathne <127718357+ravishkaw@users.noreply.github.com>
2023-11-04 11:57:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function removeLoadingSpinner() {
 async function getQuote() {
   showLoadingSpinner();
   const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
   try {
     //const response = await fetch(proxyUrl + apiUrl);
